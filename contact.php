62cfe8741c054a93bc1fe2899f44beaf129e0d9a
MukibHossain <mukib.11990@gmail.com>
2026-06-11 23:48:36 +0600

    Contact

--- contact.php
+++ contact.php
@@ -4,3 +4,53 @@ require_once 'includes/db.php';
 
 $success = "";
 $error = "";
+
+if(isset($_POST['send_message']))
+{
+    $full_name = trim($_POST['full_name']);
+    $email = trim($_POST['email']);
+    $subject = trim($_POST['subject']);
+    $message = trim($_POST['message']);
+
+    if(
+        !empty($full_name) &&
+        !empty($email) &&
+        !empty($subject) &&
+        !empty($message)
+    )
+    {
+        $stmt = $conn->prepare("
+            INSERT INTO contact_messages
+            (
+                full_name,
+                email,
+                subject,
+                message
+            )
+            VALUES
+            (?, ?, ?, ?)
+        ");
+
+        $stmt->bind_param(
+            "ssss",
+            $full_name,
+            $email,
+            $subject,
+            $message
+        );
+
+        if($stmt->execute())
+        {
+            $success = "Message sent successfully.";
+        }
+        else
+        {
+            $error = "Failed to send message.";
+        }
+    }
+    else
+    {
+        $error = "Please fill all fields.";
+    }
+}
+?>
