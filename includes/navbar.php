970e9f5b522b814b4b8b0408c2399bf83c53e380
MukibHossain <mukib.11990@gmail.com>
2026-06-18 11:40:38 +0600

    Added a drop downn menu in navbar

---
+++ includes/navbar.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Mukib Portfolio</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+</head>
+
+<body>
+
+
+<!-- SCROLL PROGRESS BAR -->
+
+<div id="progress-bar"></div>
+
+<!-- BACK TO TOP -->
+
+<button id="backToTop">
+    <i class="fas fa-arrow-up"></i>
+</button>
+
+<!-- NAVBAR -->
+
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+
+<div class="container">
+
+<a class="navbar-brand fw-bold" href="index.php">
+    Mukib
+</a>
+
+<button
+class="navbar-toggler"
+type="button"
+data-bs-toggle="collapse"
+data-bs-target="#navMenu">
+
+<span class="navbar-toggler-icon"></span>
+
+</button>
+
+<div class="collapse navbar-collapse" id="navMenu">
+
+<ul class="navbar-nav ms-auto align-items-lg-center">
+
+<li class="nav-item">
+<a class="nav-link" href="index.php#about">
+About
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="index.php#skills">
+Skills
+</a>
+</li>
+
