36f7d9b3834433ad5a614fa21b7669fd54d84357
MukibHossain <mukib.11990@gmail.com>
2026-06-18 10:31:38 +0600

    about.php page codinng started

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once 'includes/db.php';
+include 'includes/navbar.php';
+?>
+
+<section class="about-section">
+
+<div class="container">
+
+<div class="section-title text-center">
+
+<h2>About Me</h2>
+
