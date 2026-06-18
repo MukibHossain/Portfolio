92396d938fd26748bf7f271a055a8a99c5f6614f
MukibHossain <mukib.11990@gmail.com>
2026-06-18 10:32:30 +0600

    Details

--- about.php
+++ about.php
@@ -12,3 +12,17 @@ include 'includes/navbar.php';
 
 <h2>About Me</h2>
 
+<p>
+Computer Science Undergraduate • Independent Researcher
+</p>
+
+</div>
+
+<div class="row mt-5">
+
+<div class="col-lg-6">
+
+<div class="about-card">
+
+<h3>Who Am I?</h3>
+
