dc05c2e106bb4f9841ce138bea0c944fb17c8711
MukibHossain <mukib.11990@gmail.com>
2026-06-19 23:38:51 +0600

    Project page add

---
+++ projects.php
@@ -0,0 +1,17 @@
+```php
+<?php
+session_start();
+require_once 'includes/db.php';
+include 'includes/navbar.php';
+?>
+
+<section class="projects-section">
+
+<div class="container">
+
+<div class="section-title text-center">
+
+<h2>My Projects</h2>
+
+
+
